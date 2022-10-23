91c49f569abe8fd52b65435cf46ce6f79197cefb
busizhishen <66016648+busizhishen@users.noreply.github.com>
2022-10-23 16:40:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o8lPX5r3Aiq31bXJZd4uJuNmenBg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	RYqe6_pyriCua-t3VENjRwt-1bB3XBEHtZyJK1txLU8',
+      useTemplateId: 'RYqe6_pyriCua-t3VENjRwt-1bB3XBEHtZyJK1txLU8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-28',
       festivals: [
