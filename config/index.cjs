1c17e2c64e4832c49859429a21e456866f60acab
busizhishen <66016648+busizhishen@users.noreply.github.com>
2022-10-23 17:25:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '月儿宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT158122T3MJani4dyIZa5FY9w1J6QM6N',
+      id: 'SCT178184TTeVbeOftxwqIKRUaXmLHySM0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
